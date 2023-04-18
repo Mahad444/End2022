8cb3a84c3ee98b319ebd4b607e7209e32af73515
Ally Juma Said <mahadsaid95@icloud.com>
2023-04-18 23:57:22 +0100

    adding another table

--- index.sql
+++ index.sql
@@ -13,3 +13,12 @@ Create table users (
     primary key (Num_id)
 
 );
+create table data (
+    Num_id int(20) auto_increment not null,
+    firstname varchar(20) not null,
+    lastname varchar(20) not null,
+    email varchar(20) not null,
+    mypassword varchar(20) not null,
+    primary key (Num_id)
+
+);
